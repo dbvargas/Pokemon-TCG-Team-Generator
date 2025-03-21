1c9276b183095c3d8103fd6895405f54f127a0f8
brandon-lerit <brandon338@icloud.com>
2025-03-21 19:54:17 -0400

    Fixed Steel vs. Metal Issue

--- backend/app.py
+++ backend/app.py
@@ -161,7 +161,7 @@ def type_cos_sim_search(selected_types):
     data_list = list(data)
     
     type_list = ['Colorless', 'Fire', 'Water', 'Grass', 'Lightning', 'Psychic', 
-                 'Fighting', 'Darkness', 'Steel', 'Fairy', 'Dragon']
+                 'Fighting', 'Darkness', 'Metal', 'Fairy', 'Dragon']
     
     query_vec = np.zeros(len(type_list))
 
